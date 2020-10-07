a486503bf5c436c8105e843fc01ac22b6e7fffc7
Michal Szilder <michal.szilder@mohawkgp.com>
2020-10-07 14:00:35 +0200

    chunk download

--- rss_download.py
+++ rss_download.py
@@ -1,7 +1,21 @@
 import feedparser, requests, os
 
 
-#https://feeds.twit.tv/sn.xml
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+    return local_filename
+    
+#mp3 feed - https://feeds.twit.tv/sn.xml
 
 d = feedparser.parse('https://feeds.twit.tv/sn_video_hd.xml')
 fileUrl = d.entries[0].id
@@ -9,12 +23,18 @@ print(fileUrl)
 
 filename = fileUrl.split('/')[-1]
 print(filename)
-r = requests.get(fileUrl, allow_redirects=True)
 
 if not os.path.exists(filename) :
     #download file as 
     print('File %s doesn\'t exists. Downloading...' % (filename))
-    open(filename, 'wb').write(r.content)
+    
+    # this only make sense for small files that fit into memory
+    #r = requests.get(fileUrl, allow_redirects=True)
+    #open(filename, 'wb').write(r.content)
+    
+    #this method is dividing download for chunks
+    download_file(fileUrl)
 
 print('File %s exists. Opening...' % (filename))
 os.system('vlc %s' % (filename))
+
